66d0291e4c86feaf341c0addc9401f3cd26f4032
Vmolk <97118180+Vmolk@users.noreply.github.com>
2022-01-05 15:08:49 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const favicon = require('serve-favicon');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/First-Demo'));
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+app.use(express.static('./dist/SCADA_UI V0.3'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/First-Demo/'}),
-);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/SCADA_UI V0.3/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
+app.use(forceSSL());
+
 app.listen(process.env.PORT || 8080);
